99b9c4f5ef093e2d627e6abfbcea76c7d91e7eb2
DPereiraPhysics <dpereiraphysics@gmail.com>
2020-10-04 14:25:29 -0400

    Modified builder.py to create cells from .nc4

--- src/builder.py
+++ src/builder.py
@@ -5,6 +5,9 @@ from models import YearData
 from models import Data
 import json
 import uuid
+import numpy as np
+import netCDF4 as nc
+import os
 
 def id() -> str:
 	return str(uuid.uuid4())
@@ -22,25 +25,57 @@ def generateJson(data: Data):
 	print("DONE")
 
 def main():
-	# Sample data generation
-	c1 = generateCell(43.1, -79.3, 2.8, 1.9)
-	c2 = generateCell(41.6, -77.5, 2.9, 1.25)
-	monthData1 = MonthData(Month.JAN, [c1, c2])
+        monthList = [Month.JAN, Month.FEB, Month.MAR, Month.APR, Month.MAY, Month.JUN, Month.JUL, Month.AUG, Month.SEP, Month.OCT, Month.NOV, Month.DEC]
+        yearArray = []
+        
+        for year in [2018]:
+                print(year)
+                ds = nc.Dataset("../../DATA/odiac2019_1x1d_" + str(year) + ".nc")
+                lonList, latList, ffList, ibList = ds['lon'][:], ds['lat'][:], ds['land'][:], ds['intl_bunker'][:]
+                monthArray = []
 
-	c3 = generateCell(42.1, -78.3, 2.7, 1.8)
-	c4 = generateCell(41.65, -77.22, 3.9, 1.9)
-	monthData2 = MonthData(Month.FEB, [c3, c4])
-	
-	yearData1 = YearData(2018, [monthData1, monthData2])
+                for month in np.arange(12):
+                        print(monthList[month])
+                        cellArray = []
 
-	c5 = generateCell(42.3, -79.1, 2.85, 1.9)
-	c6 = generateCell(42.6, -77.2, 2.29, 1.25)
-	monthData3 = MonthData(Month.JAN, [c5, c6])
+                        for i in range(len(latList)):
+                                for j in range(len(lonList)):
+                                        lon, lat = lonList[j], latList[i]
+                                        ff_co2, ib_co2 = ffList[month, i, j], ibList[month, i, j]
+                                        c = generateCell(float(lon), float(lat), float(ff_co2), float(ib_co2))
+                                        cellArray.append(c)
 
-	c7 = generateCell(44.1, -78.05, 2.4, 1.58)
-	c8 = generateCell(45.65, -77.12, 3.14, 12.0)
-	monthData4 = MonthData(Month.FEB, [c7, c8])
+                        m = MonthData(monthList[month], cellArray)
+                        monthArray.append(m)
+
+                y = YearData(year, monthArray)
+                yearArray.append(y)
+                print("\n")
+
+        generateJson(Data(yearArray))
+        
+        '''
+        # Sample data generation
+        c1 = generateCell(43.1, -79.3, 2.8, 1.9)
+        c2 = generateCell(41.6, -77.5, 2.9, 1.25)
+        monthData1 = MonthData(Month.JAN, [c1, c2])
+
+        c3 = generateCell(42.1, -78.3, 2.7, 1.8)
+        c4 = generateCell(41.65, -77.22, 3.9, 1.9)
+        monthData2 = MonthData(Month.FEB, [c3, c4])
 	
-	yearData2 = YearData(2019, [monthData3, monthData4])
+        yearData1 = YearData(2018, [monthData1, monthData2])
+
+        c5 = generateCell(42.3, -79.1, 2.85, 1.9)
+        c6 = generateCell(42.6, -77.2, 2.29, 1.25)
+        monthData3 = MonthData(Month.JAN, [c5, c6])
+
+        c7 = generateCell(44.1, -78.05, 2.4, 1.58)
+        c8 = generateCell(45.65, -77.12, 3.14, 12.0)
+        monthData4 = MonthData(Month.FEB, [c7, c8])
 	
-	generateJson(Data([yearData1, yearData2]))
+        yearData2 = YearData(2019, [monthData3, monthData4])
+        generateJson(Data([yearData1, yearData2]))
+        '''
+
+main()
